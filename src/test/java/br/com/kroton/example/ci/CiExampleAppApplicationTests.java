ca1a0673d057de43d45a2989270a61dd78eb10b9
Rodrigo Zampieri Castilho <castilho@Rodrigos-MacBook-Pro-2.local>
2017-03-16 14:30:59 -0300

    Test update

--- src/test/java/br/com/kroton/example/ci/CiExampleAppApplicationTests.java
+++ src/test/java/br/com/kroton/example/ci/CiExampleAppApplicationTests.java
@@ -1,17 +1,34 @@
 package br.com.kroton.example.ci;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.kroton.example.ci.services.VersionApi;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CiExampleAppApplicationTests {
 
+	@Autowired
+  private VersionApi versionApi;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void get_version_successfully() throws Exception{
+		this.versionApi.getVersion();
 	}
 
 }
